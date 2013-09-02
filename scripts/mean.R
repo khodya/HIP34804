0b7543ffd78c7d86f5f34d226175f2aa16029471
saasbook <saasbook@saasbook.(none)>
2013-09-02 06:00:30 -0700

    modified mean.R

--- scripts/mean.R
+++ scripts/mean.R
@@ -1,8 +1,33 @@
 #!/usr/bin/Rscript
 
+require(get)
+
 files <-c("g074a001.dat","g074a002.dat",
 	"g074a003.dat", "g074a004.dat",
 	"g074a005.dat", "g074a006.dat",
 	"g074a007.dat", "g074a008.dat")
 
-files
+stars <- c("star_01", "star_02", "star_03", "star_04",
+      	 "star_05", "star_06", "star_07", "star_08",
+	 "star_09", "star_10", "star_11", "star_12",
+	 "star_13", "star_14", "star_15")	     
+
+stars <- c()
+num_plates <- 8
+num_stars <- 15
+
+for (i in 1:num_stars) 
+    get(stars[i]) <- c()
+
+for (i in 1:num_plates) {
+    plate <- read.table(files[i])
+    for (j in 1:num_stars) {
+        get(stars[j]) <- rbind(get(stars[j]), plate[j,10:13])
+    }
+}
+
+get(stars[1])
+
+
+
+
