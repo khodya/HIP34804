8c5bca1334c240d37a38c28e1ec6f346819a310a
Andrey Khodyrev <dukk@bk.ru>
2013-09-06 05:07:29 -0700

    ols.R release

--- scripts/ols.R
+++ scripts/ols.R
@@ -6,12 +6,9 @@ num_stars <- 15
 base_plate <- read.table(base_plate_path)
 plate <- read.table("stars")
 
-# a mean of coordinates differences
-shift <- colMeans(base_plate[,1:2] - plate[,1:2])
-
 m <- cbind(
- plate[,1] + shift[1],
- plate[,2] + shift[2],
+ plate[,1],
+ plate[,2],
  rep(1,num_stars)
 )
 
