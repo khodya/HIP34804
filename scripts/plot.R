7fef3495527edb1dd5a4509952f9f24664d38524
khodya <dukk@bk.ru>
2013-09-09 01:55:32 +0400

    plot.R release

--- scripts/plot.R
+++ scripts/plot.R
@@ -11,10 +11,16 @@ y <- c(star[1,2], starc[1,2])
 err_x <- c(star[1,3], starc[1,3])
 err_y <- c(star[1,4], starc[1,4])
 
-par(mar=c(6,6,2,2))
+par(mar=c(7,7,2,2))
 
-plotCI(x,y,err_y,pt.bg=par("bg"),pch=21,
-	xlim=c(0,2500),
-	ylim=c(0,2500),
-  	main="plotCI with extra space on the x axis")
- plotCI(x,y,err_x,pt.bg=par("bg"),pch=21,err="x",add=TRUE)
+plotCI(x,y,err_y,
+	xlim=c(0,3056),
+	ylim=c(0,3056),
+	xlab="pixels",
+	ylab="pixels",
+  	main="HIP34804\nobs. 20100101, 20110214",
+  	sub="measured coordinates",
+  	err="y",
+  	asp=1,
+  	las=1)
+ plotCI(x,y,err_x,err="x",add=TRUE)
