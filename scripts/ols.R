1c0b545ea1398078968e292c66f5a63f7284edd2
khodya <dukk@bk.ru>
2013-09-21 16:56:26 +0400

    ols.R support any number of stars

--- scripts/ols.R
+++ scripts/ols.R
@@ -1,10 +1,18 @@
 library("corpcor")
 
+max_num_ref_stars <- 15
+ms_file <- "missed_stars"
 base_plate_path <- "../20100101/stars"
-num_stars <- 15
 
-base_plate <- read.table(base_plate_path)
 plate <- read.table("stars")
+num_stars <- nrow(plate)
+
+if (file.exists(ms_file))
+	missed_stars <- read.table(ms_file) 
+	
+index <- setdiff(c(1:max_num_ref_stars),missed_stars)	
+base_src <- readLines(base_plate_path)
+base_plate <- read.table(textConnection(base_src[index]))
 
 m <- cbind(
  plate[,1],
