383d91eba8c5df9c4ea2864719d630d3b3511922
khodya <dukk@bk.ru>
2013-09-25 01:21:32 +0400

    report.R release

--- scripts/report.R
+++ scripts/report.R
@@ -1,8 +1,8 @@
 #!/usr/bin/Rscript
 
-night <- "20100101"
+base_night <- "20100101"
 nights <- dir("src")
-nights <- setdiff(nights, night)
+nights <- setdiff(nights, base_night)
 
 for (i in nights) {
 	setwd(paste0("src/",i))
