1db7fac376ee69e0e396d7763778b5ff9f23e151
khodya <dukk@bk.ru>
2013-10-14 00:24:38 +0400

    plot measured

--- scripts/plot.R
+++ scripts/plot.R
@@ -1,26 +1,23 @@
 library("plotrix")
 
-star_path <- "../20100101/star"
-m <- read.table("parameters")
+star <- read.table("reports/coordinates")
 
-star <- read.table(star_path)
-starc <- read.table("star")
-
-x <- c(star[1,1], starc[1,1])
-y <- c(star[1,2], starc[1,2])
-err_x <- c(star[1,3], starc[1,3])
-err_y <- c(star[1,4], starc[1,4])
+x <- star[,2]
+y <- star[,3]
+err_x <- star[,4]
+err_y <- star[,5]
 
 par(mar=c(7,7,2,2))
+par(pch=20)
 
 plotCI(x,y,err_y,
-	xlim=c(0,3056),
-	ylim=c(0,3056),
+	xlim=c(1739,1747),
+	ylim=c(1386,1393),
 	xlab="pixels",
 	ylab="pixels",
-  	main="HIP34804\nobs. 20100101, 20110214",
+  	main="HIP34804",
   	sub="measured coordinates",
   	err="y",
   	asp=1,
   	las=1)
- plotCI(x,y,err_x,err="x",add=TRUE)
+plotCI(x,y,err_x,err="x",add=TRUE)
