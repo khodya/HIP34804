29e2f8efc294bb8c9ec6b6eb3b0e08a71fd7cd21
khodya <dukk@bk.ru>
2013-10-25 10:57:22 +0400

    olsLm.R with residuals

--- scripts/olsLm.R
+++ scripts/olsLm.R
@@ -13,9 +13,28 @@ if (file.exists(ms_file))
 index <- setdiff(c(1:max_num_ref_stars),missed_stars)
 	
 base_src <- readLines(base_plate_path)
-base_plate <- read.table(textConnection(base_src[index]))
+base_plate <-read.table(textConnection(base_src[index]))
 
 mf_ksi <- lm(base_plate[,1] ~ plate[,1] + plate[,2],weights=1/plate[,3])
 
-mf_eta <- lm(base_plate[,2]) ~ plate[,1] + plate[,2],weights=1/plate[,4])
+mf_eta <- lm(base_plate[,2] ~ plate[,1] + plate[,2],weights=1/plate[,4])
 
+res <- cbind(
+	c(
+		as.numeric(coef(mf_ksi))[c(2,3,1)],
+		as.numeric(coef(mf_eta))[c(2,3,1)]
+	),
+	c(
+		as.numeric(coef(summary(mf_ksi))[,2][c(2,3,1)]),
+		as.numeric(coef(summary(mf_eta))[,2][c(2,3,1)])
+	)
+)
+
+res_r <- cbind(
+	as.numeric(resid(mf_ksi)),
+	as.numeric(resid(mf_eta))
+)
+
+write.table(res, file="parameters", row.names=FALSE, col.names=FALSE)
+
+write.table(res_r, file="residuals", row.names=F, col.names=F)
