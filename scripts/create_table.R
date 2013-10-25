51348e0c3c8d7bcec7db2db950cf87bc138b351e
khodya <dukk@bk.ru>
2013-10-25 11:34:28 +0400

    create_table.R commit and results

--- scripts/create_table.R
+++ scripts/create_table.R
@@ -1,5 +1,7 @@
 #!/usr/bin/Rscript
 
+# home dir: root
+
 blocked <- NULL
 base_path <- "20100101"
 nights <- dir("src")
@@ -10,20 +12,26 @@ num_stars <- nrow(base_plate)
 
 sheet <- c()
 miscl <- c()
+res_x <- c()
+res_y <- c()
 for (i in nights) { 
+	# composing stars
 	std_star <- read.table(paste0("src/",i,"/standart"))
 	sheet <- rbind(sheet, t(std_star))
 	
+	# composing residuals
 	misc_row <- rep(NA, num_stars)
 	missed <- NULL
 	if (file.exists(paste0("src/",i,"/missed_stars")))
 		missed <- read.table(paste0("src/",i,"/missed_stars"))
 	index <- setdiff(c(1:num_stars),missed)
-	misc_data <- read.table(paste0("src/",i,"/misclosures"))
+	misc_data <- read.table(paste0("src/",i,"/residuals"))
 	misc_row[index] <- misc_data[,1]
 	miscl <- rbind(miscl, misc_row)
+	res_x <- rbind(res_x, misc_row)
 	misc_row[index] <- misc_data[,2]
 	miscl <- rbind(miscl, misc_row)
+	res_y <- rbind(res_y, misc_row)
 }
 
 rownames(sheet) <- nights
@@ -35,4 +43,11 @@ rows_for_misc <- c()
 for (i in nights)
 	rows_for_misc <- cbind(rows_for_misc, c(i,i))
 rownames(miscl) <- rows_for_misc
+rownames(res_x) <- nights 
+rownames(res_y) <- nights
+
 write.table(format(round(miscl, digits=4), scientific=FALSE), file="reports/residuals", quote=FALSE, col.names=FALSE, na="NA")
+
+write.table(format(round(res_x, digits=4), scientific=FALSE), file="reports/residuals_x", quote=FALSE, col.names=FALSE, na="NA")
+
+write.table(format(round(res_y, digits=4), scientific=FALSE), file="reports/residuals_y", quote=FALSE, col.names=FALSE, na="NA")
